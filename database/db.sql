8c18dd200baa5f78a4bcf1443b027b2963a9c13c
thuany_moretti <thuany_moretti@estudante.sesisenai.org.br>
2026-08-18 08:38:25 -0300

    add - correcao do banco

--- database/db.sql
+++ database/db.sql
@@ -2,8 +2,17 @@
 CREATE DATABASE pratos_arthur_thuany_m4;
 USE pratos_arthur_thuany_m4;
 
-CREATE TABLE prato (
+CREATE TABLE pratos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(100) NOT NULL,
-    prato VARCHAR(100) NOT NULL
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
 );
